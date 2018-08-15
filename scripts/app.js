e5abbbf3f7f9b8c3efa2078b0a39a812c462cd74
natalie-poulson <natalieplsn@gmail.com>
2018-08-15 14:26:36 -0700

    completed through first bonus

--- scripts/app.js
+++ scripts/app.js
@@ -11,11 +11,18 @@ $.ajax({
 });
 
 function onSuccess (response) {
+  console.log(response);
+  initMap();
+  //markerMaker();
   var quakesArray = response.features
   $.each(quakesArray , function () {
-    var title = this.properties.title
-    $('#info').append( `<p> ${title}</p>`)
-  })
+    markerMaker(this.geometry.coordinates);
+    var title = this.properties.title 
+
+    var dt = new Date();
+    var time = Math.floor((((dt.getTime() - this.properties.time)/1000) / 60) / 60 ) ;
+    $('#info').append( `<p> ${title}, ${time} hours ago </p>`)
+  })  
 }
 
 function onError (e1, e2, e3) {
@@ -24,22 +31,38 @@ function onError (e1, e2, e3) {
   console.dir(e3)  
 }
 
+
+
 var map;
       function initMap() {
-        var myLatLong = {lat: 37.78, lng: -122.44}
+        var myLatLong = {lat:33.4771667 , lng: -116.8033333}
         map = new google.maps.Map(document.getElementById('map'), {
           center: myLatLong,
           zoom: 8
         });
-        var marker = new google.maps.Marker({
-          position: myLatLong,
-          icon: "../images/earthquake.png",
-          size: new google.maps.Size(11, 11),
-          map: map
-        });
       }
 
-initMap();
 
+function markerMaker(coord) {
+  var lat = coord[1];
+  var long = coord[0];
+  //console.log(typeOf(coord[0]));
+  //console.log(coord[0]);
+ // var myLatLong = {lat: lat, lng: long}
+ var myLatLong = {lat: lat , lng: long}
+  //console.log(myLatLong);
+  var image = {
+    url: "../images/earthquake.png",
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+  var marker = new google.maps.Marker({
+    position: myLatLong,
+    icon: image,
+    map: map
+  });
+}
 
 
